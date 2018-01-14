55203a4223420c6f921ddb4323f257fb2e4e51af
vuongbui229 <35308493+vuongbui229@users.noreply.github.com>
2018-01-14 15:19:22 +0700

    Update files.

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^post/new/$', views.post_new, name='post_new'),
+    re_path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+]
